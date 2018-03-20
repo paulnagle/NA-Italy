d72234e34ac59bdef4c5703a12613b193998982f
paulnagle <paulnagle@gmail.com>
2018-03-20 20:40:26 +0000

    Fixed Just For Today

--- src/pages/justfortoday/justfortoday-component/justfortoday.component.ts
+++ src/pages/justfortoday/justfortoday-component/justfortoday.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { LoadingController } from 'ionic-angular';
+import { App, NavController } from 'ionic-angular';
 import { JftProvider } from '../../../providers/jft/jft';
 import 'rxjs/add/operator/timeout';
 
@@ -20,16 +21,26 @@ export class JustfortodayComponent {
         });
     this.loader.present();
     this.getJFT();
+
   }
 
   getJFT(){
     this.JftProvider
       .getJFT()
       .subscribe((data)=>{
-          this.jft = data;
+
           this.loader.dismiss();
+          var elem = document.createElement('div');
+
+          elem.innerHTML = data;
+
+          var tmp = elem.getElementsByClassName("region region-content");
+
+          this.jft = tmp[0].innerHTML;
+
         }
-      );
+    );
+
   }
 
 }
