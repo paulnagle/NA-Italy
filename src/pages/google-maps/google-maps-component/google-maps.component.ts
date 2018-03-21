01cd84a2ef9fecd97bf2f4deddf987f288ccc899
paulnagle <paulnagle@gmail.com>
2018-03-21 14:14:12 +0000

    If lat/longs overlap. move one by a couple of meters

--- src/pages/google-maps/google-maps-component/google-maps.component.ts
+++ src/pages/google-maps/google-maps-component/google-maps.component.ts
@@ -22,6 +22,8 @@ export class GoogleMapsComponent {
     this.loader.present();
 
     this.getAllMeetings();
+
+
   }
 
   // Google Map zoom level
@@ -42,6 +44,19 @@ export class GoogleMapsComponent {
       this.meetingList  = this.meetingList.filter(meeting => meeting.longitude = parseFloat(meeting.longitude));
       this.meetingList  = this.meetingList.filter(meeting => meeting.start_time = (meeting.start_time).substring(0,5));
       this.meetingList  = this.meetingList.filter(meeting => meeting.weekday_tinyint = this.dayOfWeekAsString(meeting.weekday_tinyint));
+
+      //check to see if any of the  markers overlap.. meetings are sorted by long first, then lat
+      // If lat/longs overlap, move one by a couple of meters
+      var i : any;
+      for (i=0; i < this.meetingList.length -1; i++) {
+
+          if (this.meetingList[i].longitude == this.meetingList[i+1].longitude) {
+            if (this.meetingList[i].latitude == this.meetingList[i+1].latitude){
+              this.meetingList[i].longitude = this.meetingList[i].longitude  + (Math.random() -.8) / 1500;
+              this.meetingList[i].latitude = this.meetingList[i].latitude  + (Math.random() -.8) / 1500;
+            }
+          }
+      }
 
       this.loader.dismiss();
     });
