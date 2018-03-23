bee95d884cd5d884f4887bf374c9e83e2a83b891
paulnagle <paulnagle@gmail.com>
2018-03-23 13:01:14 +0000

    Better fix for overlapping markers

--- src/pages/google-maps/google-maps-component/google-maps.component.ts
+++ src/pages/google-maps/google-maps-component/google-maps.component.ts
@@ -45,20 +45,43 @@ export class GoogleMapsComponent {
       this.meetingList  = this.meetingList.filter(meeting => meeting.start_time = (meeting.start_time).substring(0,5));
       this.meetingList  = this.meetingList.filter(meeting => meeting.weekday_tinyint = this.dayOfWeekAsString(meeting.weekday_tinyint));
 
-      //check to see if any of the  markers overlap.. meetings are sorted by long first, then lat
+      // check to see if any of the  markers overlap..
+      // meetings are sorted by long first, then lat
       // If lat/longs overlap, move one by a couple of meters
       var i : any;
       for (i=0; i < this.meetingList.length -1; i++) {
-        if (this.meetingList[i].longitude == this.meetingList[i+1].longitude) {
-          if (this.meetingList[i].latitude == this.meetingList[i+1].latitude) {
-            this.meetingList[i].longitude = this.meetingList[i].longitude  + (Math.random() / 15000);
+        var longOffset : any = 0;
+        var latOffset : any = 0;
+        var Offset : any = 0.00002;
+
+        while ((this.meetingList[i].longitude == this.meetingList[i+1].longitude) &&
+               (this.meetingList[i].latitude == this.meetingList[i+1].latitude) ){
+
+          if ( (i % 2) === 1) {
+            longOffset += Offset;
+            this.meetingList[i].longitude = this.meetingList[i].longitude  + longOffset;
+          } else {
+            latOffset += Offset;
+            this.meetingList[i].latitude = this.meetingList[i].latitude  + latOffset;
           }
-        }
-      }
+
+          i++;
+
+          if (i == (this.meetingList.length - 1)) {
+            longOffset = 0;
+            latOffset = 0;
+            break;
+          }
+
+        } // while
+
+      } // for
 
       this.loader.dismiss();
     });
   }
+
+
 
   public openMapsLink(destLatitude, destLongitude) {
     // ios
