69d05e37657d16e5b49b0cd17a8cabe021c0a98e
paulnagle <paulnagle@gmail.com>
2018-04-15 12:12:48 +0100

    Events loader translation

--- src/pages/events/events-component/events.component.ts
+++ src/pages/events/events-component/events.component.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
-import { LoadingController } from 'ionic-angular';
-import { App, NavController } from 'ionic-angular';
-import { EventsProvider } from "../../../providers/events/events";
+import { Component }            from '@angular/core';
+import { LoadingController }    from 'ionic-angular';
+import { App, NavController }   from 'ionic-angular';
+import { EventsProvider }       from "../../../providers/events/events";
 import 'rxjs/add/operator/timeout';
+import { TranslateService }     from '@ngx-translate/core';
+
 
 @Component({
   selector: 'page-events',
@@ -14,36 +16,48 @@ export class EventsComponent {
    events : string;
    loader = null;
 
-  constructor(private EventsProvider : EventsProvider , public loadingCtrl: LoadingController) {
-    this.loader = this.loadingCtrl.create({
-          content: "Caricamento eventi...",
-          duration: 10000
-        });
-    this.loader.present();
+  constructor ( private EventsProvider  : EventsProvider ,
+                public loadingCtrl      : LoadingController,
+                private translate       : TranslateService ) {
+    // EventsComponent constructor
+    this.translate.get('EVENTSLOADER').subscribe(
+      value => {
+        // value is our translated string
+        this.presentLoader(value);
+      }
+    )
     this.getEvents();
-
   }
 
   getEvents(){
     this.EventsProvider
       .getEvents()
       .subscribe((data)=>{
-
           this.loader.dismiss();
           var elem = document.createElement('div');
-
           elem.innerHTML = data;
-
           var a = elem.getElementsByTagName("A");
           for(var i=0;i<a.length;i++)
               a[i].removeAttribute("href");
-
           var tmp = elem.getElementsByClassName("view-content");
-
           this.events= tmp[0].innerHTML;
         }
     );
-
   }
 
+  presentLoader(loaderText) {
+    if (!this.loader) {
+      this.loader = this.loadingCtrl.create({
+        content: loaderText
+      });
+      this.loader.present();
+    }
+  }
+
+  dismissLoader() {
+    if(this.loader){
+      this.loader.dismiss();
+      this.loader = null;
+    }
+  }
 }
