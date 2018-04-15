5d62515b90be029b731d5b3d45059d1e8a9a90c4
paulnagle <paulnagle@gmail.com>
2018-04-15 10:31:02 +0100

    Map Search translations setup

--- src/pages/google-maps/google-maps-component/google-maps.component.ts
+++ src/pages/google-maps/google-maps-component/google-maps.component.ts
@@ -5,6 +5,7 @@ import { ToastController } from 'ionic-angular';
 import { Geolocation } from '@ionic-native/geolocation';
 import { MouseEvent, LatLngLiteral, LatLngBounds, AgmCircle , AgmMap } from '@agm/core';
 import { MeetingListProvider } from '../../../providers/meeting-list/meeting-list';
+import { TranslateService } from '@ngx-translate/core';
 
 declare const google: any;
 
@@ -24,13 +25,18 @@ export class GoogleMapsComponent {
   @ViewChild('circle', {read: AgmCircle}) circle: AgmCircle;
   mapBounds    : LatLngBounds;
 
-  constructor(private MeetingListProvider : MeetingListProvider,
-              public  loadingCtrl         : LoadingController,
-              public  plt                 : Platform,
-              private geolocation         : Geolocation,
-              private toastCtrl: ToastController ) {}
+  constructor ( private MeetingListProvider : MeetingListProvider,
+                public  loadingCtrl         : LoadingController,
+                public  plt                 : Platform,
+                private geolocation         : Geolocation,
+                private toastCtrl           : ToastController,
+                private translate           : TranslateService ) {
+  // Constructor for GoogleMapsComponent
+
+  }
 
   mapReady(event: any) {
+    console.log("Entering mapReady: ")
     this.map = event;
     this.radius = 50;
     this.radiusMeters = this.radius * 1000;
@@ -38,15 +44,18 @@ export class GoogleMapsComponent {
     this.map.controls[google.maps.ControlPosition.TOP_CENTER].push(document.getElementById('RadiusRange'));
   }
 
-  dayOfWeekAsString(dayIndex) {
-  	return ["not a day?", "Do", "Lun","Mar","Mer","Gio","Ven","Sab"][dayIndex];
-  }
-
   getCircleMeetings(event){
+    console.log("Entering getCircleMeetings: ")
+
     if (typeof event === "undefined") {  // spurious event, don't run a search
       return;
     }
-    this.presentLoader("Caricamento mappa...");
+    this.translate.get('LOADINGMAP').subscribe(
+      value => {
+        // value is our translated string
+        this.presentLoader(value);
+      }
+    )
     this.MeetingListProvider.getCircleMeetings(this.latitude, this.longitude, this.radius).subscribe((data)=>{
       if (JSON.stringify(data) == "{}") {  // empty result set!
         this.meetingList = JSON.parse("[]");
@@ -54,8 +63,7 @@ export class GoogleMapsComponent {
         this.meetingList  = data;
         this.meetingList  = this.meetingList.filter(meeting => meeting.latitude = parseFloat(meeting.latitude));
         this.meetingList  = this.meetingList.filter(meeting => meeting.longitude = parseFloat(meeting.longitude));
-        this.meetingList  = this.meetingList.filter(meeting => meeting.start_time = (meeting.start_time).substring(0,5));
-        this.meetingList  = this.meetingList.filter(meeting => meeting.weekday_tinyint = this.dayOfWeekAsString(meeting.weekday_tinyint));
+        console.log(this.meetingList);
       }
         var i : any;
         for (i = 0; i < this.meetingList.length - 1; i++) {
@@ -85,6 +93,8 @@ export class GoogleMapsComponent {
   }
 
   circleDragEnd($event: MouseEvent) {
+    console.log("Entering circleDragEnd: ")
+
     this.latitude = $event.coords.lat;
     this.longitude = $event.coords.lng;
     this.latitude = parseFloat(this.latitude);
@@ -99,6 +109,8 @@ export class GoogleMapsComponent {
   }
 
   circleRadiusChange(event: any) {
+    console.log("Entering circleRadiusChange: ")
+
     this.circle.getBounds().then((bounds) => {
       this.mapBounds =  bounds;
     })
@@ -110,6 +122,8 @@ export class GoogleMapsComponent {
   }
 
   public openMapsLink(destLatitude, destLongitude) {
+    console.log("Entering openMapsLink: ")
+
     // ios
     if (this.plt.is('ios')) {
       window.open('https://www.google.com/maps/search/?api=1&query=' + destLatitude + ',' + destLongitude + ')', '_system');
@@ -137,7 +151,14 @@ export class GoogleMapsComponent {
   }
 
   locatePhone() {
-    this.presentLoader("Trovare il telefono");
+    console.log("Entering locatePhone: ")
+    this.translate.get('LOCATING').subscribe(
+      value => {
+        // value is our translated string
+        this.presentLoader(value);
+      }
+    )
+
     this.geolocation.getCurrentPosition({timeout: 5000}).then((resp) => {
       this.latitude = resp.coords.latitude;
       this.longitude = resp.coords.longitude;
